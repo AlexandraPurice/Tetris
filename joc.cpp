260d9e6ee7c368bc3c2ba293448187cdc2a1d0e3
PuriceAlexandra <aalexaandraa497@gmail.com>
2017-01-13 19:56:21 +0200

    commit4

--- joc.cpp
+++ joc.cpp
@@ -2,24 +2,30 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
 #include <sstream>
+
 using namespace sf;
+
 const int M = 20;
 const int N = 10;
+
 int field[M][N] = { 0 };
+
 struct Point
 {
 	int x, y;
 } a[4], b[4];
+
 int figures[7][4] =
 {
-	1,3,5,7, // I
-	2,4,5,7, // Z
-	3,5,4,6, // S
-	3,5,4,7, // T
-	2,3,5,7, // L
-	3,5,7,6, // J
-	2,3,4,5, // O
+	1,3,5,7,
+	2,4,5,7,
+	3,5,4,6,
+	3,5,4,7,
+	2,3,5,7,
+	3,5,7,6,
+	2,3,4,5,
 };
+
 bool check()
 {
 	int i;
@@ -32,7 +38,9 @@ bool check()
 
 	return 1;
 };
+
 int score, t;
+
 int main()
 {
 	int i, j, n, colorNum, dx;
@@ -41,44 +49,21 @@ int main()
 	srand(time(0));
 
 	RenderWindow window(VideoMode(320, 480), "TETRIS");
-
 	Texture t1, t2, t3;
 	t1.loadFromFile("C:/Users/Alexandra/Desktop/imagini/tiles.png");
 	t2.loadFromFile("C:/Users/Alexandra/Desktop/imagini/background.png");
 	t3.loadFromFile("C:/Users/Alexandra/Desktop/imagini/frame.png");
-
 	Sprite s(t1), background(t2), frame(t3);
-
 	dx = 0;
 	rotate = 0;
-	colorNum = 1;
+	colorNum = 0;
 	timer = 0, delay = 0.3;
-
 	Clock clock;
-
 	while (window.isOpen())
 	{
 		float time = clock.getElapsedTime().asSeconds();
 		clock.restart();
 		timer += time;
-		for (j = 0; j < M; j++)
-			if (field[0][j] == 1)
-			{
-
-				sf::Font arial;
-				arial.loadFromFile("arial.ttf");
-
-				std::ostringstream end;
-				//end << "GAME OVER";
-				end << "Score: " << std::endl;
-				sf::Text FINALGAMES;
-
-				FINALGAMES.setCharacterSize(50);
-				FINALGAMES.setPosition({ 200, 200 });
-				FINALGAMES.setFont(arial);
-				FINALGAMES.setString(end.str());
-			}
-
 		Event e;
 		while (window.pollEvent(e))
 		{
@@ -95,10 +80,8 @@ int main()
 						if (e.key.code == Keyboard::Right)
 							dx = 1;
 		}
-
 		if (Keyboard::isKeyPressed(Keyboard::Down))
 			delay = 0.05;
-
 		//MUTARE
 		for (i = 0; i<4; i++)
 		{
@@ -108,11 +91,10 @@ int main()
 		if (!check())
 			for (i = 0; i<4; i++)
 				a[i] = b[i];
-
-		//ROTARE PIESE
+		//ROTIRE PIESE
 		if (rotate)
 		{
-			Point p = a[1]; //center of rotation
+			Point p = a[1];
 			for (i = 0; i<4; i++)
 			{
 				int x = a[i].y - p.y;
@@ -124,21 +106,41 @@ int main()
 				for (i = 0; i<4; i++)
 					a[i] = b[i];
 		}
+		//Tick
+		if (timer>delay)
+		{
+			for (i = 0; i<4; i++)
+			{
+				b[i] = a[i];
+				a[i].y += 1;
+			}
+
+			if (!check())
+			{
+				for (i = 0; i<4; i++)
+					field[b[i].y][b[i].x] = colorNum;
+
+				colorNum = 1 + rand() % 7;
+				n = rand() % 7;
+				for (i = 0; i<4; i++)
+				{
+					a[i].x = figures[n][i] % 2;
+					a[i].y = figures[n][i] / 2;
+				}
+			}
+
+			timer = 0;
+		}
 
 		//score
-		//int score = 0;
-
 		sf::Font arial;
 		arial.loadFromFile("arial.ttf");
-
 		std::ostringstream ssScore;
 		ssScore << "Score: " << std::endl;
 		ssScore << score;
-
 		sf::Text lblScore;
-
 		lblScore.setCharacterSize(20);
-		lblScore.setPosition({ 220, 200 });
+		lblScore.setPosition({ 250, 100 });
 		lblScore.setFont(arial);
 		lblScore.setString(ssScore.str());
 		//elimina linie
@@ -151,8 +153,6 @@ int main()
 				if (field[i][j])
 					count++;
 				field[k][j] = field[i][j];
-
-
 				if (count >= N - 1 + 1)
 				{
 					t++;
@@ -178,13 +178,57 @@ int main()
 				ssScore << "Score " << score;
 				lblScore.setString(ssScore.str());
 			}
+
 			if (count < N)
 				k--;
 		}
 		dx = 0; rotate = 0; delay = 0.3;
+		//draw
+		window.clear(Color::White);
+		window.draw(background);
 		window.draw(frame);
+		for (i = 0; i<M; i++)
+			for (j = 0; j < N; j++)
+			{
+				if (field[i][j] == 0)
+					continue;
+				s.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
+				s.setPosition(j * 18, i * 18);
+				s.move(28, 31);
+				window.draw(s);
+			}
+		for (int i = 0; i < M; i++)
+		{
+			if (field[0][i] != 0)
+				for (int j = i; j < N; j++)
+					if (field[i][j] != 0)
+					{
+						sf::Font arial;
+						arial.loadFromFile("arial.ttf");
+
+						std::ostringstream gameover;
+						gameover << "GAME OVER ";
+						sf::Text lblGame;
+
+						lblGame.setCharacterSize(30);
+						lblGame.setPosition({ 100, 200 });
+						lblGame.setFont(arial);
+						lblGame.setString(gameover.str());
+						window.draw(lblGame);
+
+					}
+		}
+		for (i = 0; i<4; i++)
+		{
+			s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
+			s.setPosition(a[i].x * 18, a[i].y * 18);
+			s.move(28, 31);
+			window.draw(s);
+		}
+
 		window.draw(lblScore);
 		window.display();
 	}
+
 	return 0;
 }
